b7640fb14b945a2a45c68e794ce8f7805ee2ca8d
Exicetin <77433018+Exicetin@users.noreply.github.com>
2022-04-07 13:01:38 +0300

    Add files via upload

---
+++ NewUser.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RpS2._0
+{
+
+    public partial class NewUser : Window
+    {
+        public NewUser()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private void New_Report_Click(object sender, RoutedEventArgs e)
+        {
+            NewReport taskWindow = new NewReport();
+            taskWindow.Show();
+            this.Close();
+        }
+    }
+}
